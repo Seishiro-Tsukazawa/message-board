dc1a6e897b5145ffd6c151c03a5739daf5730d78
seishiro.tsukazawa <nantefireda0302@gmail.com>
2020-02-28 01:28:25 +0000

    complete_messega-board

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+    before_action :set_message, only: [:show, :edit, :update, :destroy]
     def index
-        @messages = Message.all
+        @messages = Message.order(id: :desc).page(params[:page])
     end
     
     def show
-        @message = Message.find(params[:id])
+        
     end
     
     def new
@@ -24,25 +25,33 @@ class MessagesController < ApplicationController
     end
     
     def edit
-        @message = Message.find(params[:id])
+        
     end
     
     def update
-        @message = Message.find(params[:id])
-
         if @message.update(message_params)
             flash[:success] = 'Message は正常に更新されました'
             redirect_to @message
         else
             flash.now[:danger] = 'Message は更新されませんでした'
             render :edit
+        end
     end
     
     def destroy
-        @message = Message.find(params[:id])
         @message.destroy
 
         flash[:success] = 'Message は正常に削除されました'
         redirect_to messages_url
+    end
+    
+    private
+    
+    def set_message
+        @message = Message.find(params[:id])
+    end
+    
+    def message_params
+        params.require(:message).permit(:content, :title)
     end
 end
